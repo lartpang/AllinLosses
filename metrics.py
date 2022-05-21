5ac007fe2c5c21b9e2313a59a09b902e199a1a55
axjing <34448205+axjing@users.noreply.github.com>
2022-05-21 12:15:53 +0800

    Create metrics.py

---
+++ metrics.py
@@ -0,0 +1,616 @@
+# python3
+# @File: metrics_evaluation.py
+# --coding:utf-8--
+# @Author:axjing
+# @Time: 2021年12月14日15
+# 说明:
+#    Copyright 2020 Division of Medical Image Computing, German Cancer Research Center (DKFZ), Heidelberg, Germany
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import numpy as np
+from medpy import metric
+import torch
+from utils.one_hot import mask_one_hot
+
+
+def assert_shape(test, reference):
+    assert test.shape == reference.shape, "Shape mismatch: {} and {}".format(
+        test.shape, reference.shape)
+
+
+class ConfusionMatrix:
+    """解混线矩阵"""
+
+    def __init__(self, test=None, reference=None):
+
+        self.tp = None
+        self.fp = None
+        self.tn = None
+        self.fn = None
+        self.size = None
+        self.reference_empty = None
+        self.reference_full = None
+        self.test_empty = None
+        self.test_full = None
+        self.set_reference(reference)
+        self.set_test(test)
+
+    def set_test(self, test):
+
+        self.test = test
+        self.reset()
+
+    def set_reference(self, reference):
+
+        self.reference = reference
+        self.reset()
+
+    def reset(self):
+
+        self.tp = None
+        self.fp = None
+        self.tn = None
+        self.fn = None
+        self.size = None
+        self.test_empty = None
+        self.test_full = None
+        self.reference_empty = None
+        self.reference_full = None
+
+    def compute(self):
+
+        if self.test is None or self.reference is None:
+            raise ValueError("'test' and 'reference' must both be set to compute confusion matrix.")
+
+        assert_shape(self.test, self.reference)
+
+        self.tp = int(((self.test != 0) * (self.reference != 0)).sum())
+        self.fp = int(((self.test != 0) * (self.reference == 0)).sum())
+        self.tn = int(((self.test == 0) * (self.reference == 0)).sum())
+        self.fn = int(((self.test == 0) * (self.reference != 0)).sum())
+        self.size = int(np.prod(self.reference.shape, dtype=np.int64))
+        self.test_empty = not np.any(self.test)
+        self.test_full = np.all(self.test)
+        self.reference_empty = not np.any(self.reference)
+        self.reference_full = np.all(self.reference)
+
+    def get_matrix(self):
+
+        for entry in (self.tp, self.fp, self.tn, self.fn):
+            if entry is None:
+                self.compute()
+                break
+
+        return self.tp, self.fp, self.tn, self.fn
+
+    def get_size(self):
+
+        if self.size is None:
+            self.compute()
+        return self.size
+
+    def get_existence(self):
+
+        for case in (self.test_empty, self.test_full, self.reference_empty, self.reference_full):
+            if case is None:
+                self.compute()
+                break
+
+        return self.test_empty, self.test_full, self.reference_empty, self.reference_full
+
+
+def dice_to_binaryv1(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, **kwargs):
+    """2TP / (2TP + FP + FN)"""
+
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    tp, fp, tn, fn = confusion_matrix.get_matrix()
+    test_empty, test_full, reference_empty, reference_full = confusion_matrix.get_existence()
+
+    if test_empty and reference_empty:
+        if nan_for_nonexisting:
+            return float("NaN")
+        else:
+            return 0.
+
+    return float(2. * tp / (2 * tp + fp + fn))
+
+
+def jaccard(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, **kwargs):
+    """TP / (TP + FP + FN)"""
+
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    tp, fp, tn, fn = confusion_matrix.get_matrix()
+    test_empty, test_full, reference_empty, reference_full = confusion_matrix.get_existence()
+
+    if test_empty and reference_empty:
+        if nan_for_nonexisting:
+            return float("NaN")
+        else:
+            return 0.
+
+    return float(tp / (tp + fp + fn))
+
+
+def precision(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, **kwargs):
+    """TP / (TP + FP)"""
+
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    tp, fp, tn, fn = confusion_matrix.get_matrix()
+    test_empty, test_full, reference_empty, reference_full = confusion_matrix.get_existence()
+
+    if test_empty:
+        if nan_for_nonexisting:
+            return float("NaN")
+        else:
+            return 0.
+
+    return float(tp / (tp + fp))
+
+
+def sensitivity(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, **kwargs):
+    """TP / (TP + FN)"""
+
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    tp, fp, tn, fn = confusion_matrix.get_matrix()
+    test_empty, test_full, reference_empty, reference_full = confusion_matrix.get_existence()
+
+    if reference_empty:
+        if nan_for_nonexisting:
+            return float("NaN")
+        else:
+            return 0.
+
+    return float(tp / (tp + fn))
+
+
+def recall(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, **kwargs):
+    """TP / (TP + FN)"""
+
+    return sensitivity(test, reference, confusion_matrix, nan_for_nonexisting, **kwargs)
+
+
+def specificity(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, **kwargs):
+    """TN / (TN + FP)"""
+
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    tp, fp, tn, fn = confusion_matrix.get_matrix()
+    test_empty, test_full, reference_empty, reference_full = confusion_matrix.get_existence()
+
+    if reference_full:
+        if nan_for_nonexisting:
+            return float("NaN")
+        else:
+            return 0.
+
+    return float(tn / (tn + fp))
+
+
+def accuracy(test=None, reference=None, confusion_matrix=None, **kwargs):
+    """(TP + TN) / (TP + FP + FN + TN)"""
+
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    tp, fp, tn, fn = confusion_matrix.get_matrix()
+
+    return float((tp + tn) / (tp + fp + tn + fn))
+
+
+def fscore(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, beta=1., **kwargs):
+    """(1 + b^2) * TP / ((1 + b^2) * TP + b^2 * FN + FP)"""
+
+    precision_ = precision(test, reference, confusion_matrix, nan_for_nonexisting)
+    recall_ = recall(test, reference, confusion_matrix, nan_for_nonexisting)
+
+    try:
+        score_ret = (1 + beta * beta) * precision_ * recall_ / \
+                    ((beta * beta * precision_) + recall_)
+    except ZeroDivisionError:
+        score_ret = 0.0
+    return score_ret
+
+
+def false_positive_rate(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, **kwargs):
+    """FP / (FP + TN)"""
+
+    return 1 - specificity(test, reference, confusion_matrix, nan_for_nonexisting)
+
+
+def false_omission_rate(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, **kwargs):
+    """FN / (TN + FN)"""
+
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    tp, fp, tn, fn = confusion_matrix.get_matrix()
+    test_empty, test_full, reference_empty, reference_full = confusion_matrix.get_existence()
+
+    if test_full:
+        if nan_for_nonexisting:
+            return float("NaN")
+        else:
+            return 0.
+
+    return float(fn / (fn + tn))
+
+
+def false_negative_rate(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, **kwargs):
+    """FN / (TP + FN)"""
+
+    return 1 - sensitivity(test, reference, confusion_matrix, nan_for_nonexisting)
+
+
+def true_negative_rate(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, **kwargs):
+    """TN / (TN + FP)"""
+
+    return specificity(test, reference, confusion_matrix, nan_for_nonexisting)
+
+
+def false_discovery_rate(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, **kwargs):
+    """FP / (TP + FP)"""
+
+    return 1 - precision(test, reference, confusion_matrix, nan_for_nonexisting)
+
+
+def negative_predictive_value(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, **kwargs):
+    """TN / (TN + FN)"""
+
+    return 1 - false_omission_rate(test, reference, confusion_matrix, nan_for_nonexisting)
+
+
+def total_positives_test(test=None, reference=None, confusion_matrix=None, **kwargs):
+    """TP + FP"""
+
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    tp, fp, tn, fn = confusion_matrix.get_matrix()
+
+    return tp + fp
+
+
+def total_negatives_test(test=None, reference=None, confusion_matrix=None, **kwargs):
+    """TN + FN"""
+
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    tp, fp, tn, fn = confusion_matrix.get_matrix()
+
+    return tn + fn
+
+
+def total_positives_reference(test=None, reference=None, confusion_matrix=None, **kwargs):
+    """TP + FN"""
+
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    tp, fp, tn, fn = confusion_matrix.get_matrix()
+
+    return tp + fn
+
+
+def total_negatives_reference(test=None, reference=None, confusion_matrix=None, **kwargs):
+    """TN + FP"""
+
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    tp, fp, tn, fn = confusion_matrix.get_matrix()
+
+    return tn + fp
+
+
+def hausdorff_distance(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, voxel_spacing=None,
+                       connectivity=1, **kwargs):
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    test_empty, test_full, reference_empty, reference_full = confusion_matrix.get_existence()
+
+    if test_empty or test_full or reference_empty or reference_full:
+        if nan_for_nonexisting:
+            return float("NaN")
+        else:
+            return 0
+
+    test, reference = confusion_matrix.test, confusion_matrix.reference
+
+    return metric.hd(test, reference, voxel_spacing, connectivity)
+
+
+def hausdorff_distance_95(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True,
+                          voxel_spacing=None, connectivity=1, **kwargs):
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    test_empty, test_full, reference_empty, reference_full = confusion_matrix.get_existence()
+
+    if test_empty or test_full or reference_empty or reference_full:
+        if nan_for_nonexisting:
+            return float("NaN")
+        else:
+            return 0
+
+    test, reference = confusion_matrix.test, confusion_matrix.reference
+
+    return metric.hd95(test, reference, voxel_spacing, connectivity)
+
+
+def avg_surface_distance(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True, voxel_spacing=None,
+                         connectivity=1, **kwargs):
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    test_empty, test_full, reference_empty, reference_full = confusion_matrix.get_existence()
+
+    if test_empty or test_full or reference_empty or reference_full:
+        if nan_for_nonexisting:
+            return float("NaN")
+        else:
+            return 0
+
+    test, reference = confusion_matrix.test, confusion_matrix.reference
+
+    return metric.asd(test, reference, voxel_spacing, connectivity)
+
+
+def avg_surface_distance_symmetric(test=None, reference=None, confusion_matrix=None, nan_for_nonexisting=True,
+                                   voxel_spacing=None, connectivity=1, **kwargs):
+    if confusion_matrix is None:
+        confusion_matrix = ConfusionMatrix(test, reference)
+
+    test_empty, test_full, reference_empty, reference_full = confusion_matrix.get_existence()
+
+    if test_empty or test_full or reference_empty or reference_full:
+        if nan_for_nonexisting:
+            return float("NaN")
+        else:
+            return 0
+
+    test, reference = confusion_matrix.test, confusion_matrix.reference
+
+    return metric.assd(test, reference, voxel_spacing, connectivity)
+
+
+ALL_METRICS = {
+    "False Positive Rate": false_positive_rate,
+    "Dice": dice_to_binaryv1,
+    "Jaccard": jaccard,
+    "Hausdorff Distance": hausdorff_distance,
+    "Hausdorff Distance 95": hausdorff_distance_95,
+    "Precision": precision,
+    "Recall": recall,
+    "Avg. Symmetric Surface Distance": avg_surface_distance_symmetric,
+    "Avg. Surface Distance": avg_surface_distance,
+    "Accuracy": accuracy,
+    "False Omission Rate": false_omission_rate,
+    "Negative Predictive Value": negative_predictive_value,
+    "False Negative Rate": false_negative_rate,
+    "True Negative Rate": true_negative_rate,
+    "False Discovery Rate": false_discovery_rate,
+    "Total Positives Test": total_positives_test,
+    "Total Negatives Test": total_negatives_test,
+    "Total Positives Reference": total_positives_reference,
+    "total Negatives Reference": total_negatives_reference
+}
+
+
+def recal_to_binary(predict, target):  # Sensitivity, Recall, true positive rate都一样
+    if torch.is_tensor(predict):
+        # predict = torch.sigmoid(predict).data.cpu().numpy()
+        predict = predict.detach().cpu().numpy()
+    if torch.is_tensor(target):
+        target = target.data.cpu().numpy()
+
+    predict = np.atleast_1d(predict.astype(np.bool))
+    target = np.atleast_1d(target.astype(np.bool))
+
+    tp = np.count_nonzero(predict & target)
+    fn = np.count_nonzero(~predict & target)
+
+    try:
+        recall = tp / float(tp + fn)
+    except ZeroDivisionError:
+        recall = 0.0
+
+    return recall
+
+
+def per_dice(output, target, eps=1e-10):  # soft dice loss
+    # target = target.float()
+    num = 2 * (output * target).sum() + eps
+    den = output.sum() + target.sum() + eps
+
+    return num / den
+
+
+def dice_sorces(output, target, eps=1e-5):
+    """
+
+    :param output:  [bsize,c,H,W,D]
+    :param target:  [bsize,H,W,D]
+    :param eps: 平滑系数，防止除0
+    :return:dsc dice系数
+    注意：
+    每一个c代表一个类别，因为使用了for循环，如果target中的类别不是0，1,2,3等连续的情况请处理
+    """
+    dsc = []
+    for channel in range(1, output.shape[1]):
+        # for channel in range(output.shape[1]):
+        dsc_ = per_dice(output[:, channel, ...], (target == channel).float(), eps=eps)
+        dsc.append(float(dsc_))
+    return dsc
+
+
+def dice_to_binary(predict, target):
+    smooth = 1e-10
+    if torch.is_tensor(predict):
+        # predict = torch.sigmoid(predict).data.cpu().numpy()
+        predict = predict.detach().cpu().numpy()
+    if torch.is_tensor(target):
+        target = target.detach().cpu().numpy()
+
+    predict = np.atleast_1d(predict.astype(np.bool))  # 转一维数组
+    target = np.atleast_1d(target.astype(np.bool))
+
+    intersection = np.count_nonzero(predict & target)  # 计算非零个数
+
+    size_i1 = np.count_nonzero(predict)
+    size_i2 = np.count_nonzero(target)
+
+    try:
+        dice = (2. * intersection + smooth) / (float(size_i1 + size_i2) + smooth)
+    except ZeroDivisionError:
+        if intersection == 0 and float(size_i1 + size_i2) == 0:
+            dice = 1.0
+        else:
+            dice = 0.0
+
+    return dice
+
+
+def specificity_to_binary(predict, target):  # Specificity，true negative rate一样
+    if torch.is_tensor(predict):
+        predict = predict.detach().cpu().numpy()
+    if torch.is_tensor(target):
+        target = target.data.cpu().numpy()
+
+    predict = np.atleast_1d(predict.astype(np.bool))
+    target = np.atleast_1d(target.astype(np.bool))
+
+    tn = np.count_nonzero(~predict & ~target)
+    fp = np.count_nonzero(predict & ~target)
+
+    try:
+        spec = tn / float(tn + fp)
+    except ZeroDivisionError:
+        spec = 0.0
+
+    return spec
+
+
+def precision_to_binary(predict, target):
+    if torch.is_tensor(predict):
+        predict = predict.detach().cpu().numpy()
+    if torch.is_tensor(target):
+        target = target.data.cpu().numpy()
+
+    predict = np.atleast_1d(predict.astype(np.bool))
+    target = np.atleast_1d(target.astype(np.bool))
+
+    tp = np.count_nonzero(predict & target)
+    fp = np.count_nonzero(predict & ~target)
+
+    try:
+        precision = tp / float(tp + fp)
+    except ZeroDivisionError:
+        precision = 0.0
+
+    return precision
+
+
+def jaccard_to_binary(predict, target):
+    """
+    $$jac=dice/(2-dice)$$
+    :param predict:
+    :param target:
+    :return:
+    """
+    if torch.is_tensor(predict):
+        predict = predict.detach().cpu().numpy()
+    if torch.is_tensor(target):
+        target = target.data.cpu().numpy()
+
+    predict = np.atleast_1d(predict.astype(np.bool))
+    target = np.atleast_1d(target.astype(np.bool))
+
+    intersection = np.count_nonzero(predict & target)
+    union = np.count_nonzero(predict | target)
+
+    # jac = float(intersection) / float(union)
+    try:
+        jac = float(intersection) / float(union)
+    except ZeroDivisionError:
+        jac = 0.0
+
+    return jac
+
+
+def accuracy_to_binary(pred, gt):
+    """(TP + TN) / (TP + FP + FN + TN)"""
+
+    N = gt.size(0)
+    pred_flat = pred.view(N, -1)
+    gt_flat = gt.view(N, -1)
+    tp = torch.sum((pred_flat != 0) * (gt_flat != 0))
+    fp = torch.sum((pred_flat != 0) * (gt_flat == 0))
+    tn = torch.sum((pred_flat == 0) * (gt_flat == 0))
+    fn = torch.sum((pred_flat == 0) * (gt_flat != 0))
+
+    score = (tp + tn).float() / (tp + fp + tn + fn).float()
+
+    return score.sum() / N
+
+
+def calculate_metric_percase(predict, target):
+    if predict.sum() > 0 and target.sum() > 0:
+        dice = metric.binary.dc(predict, target)
+        jc = metric.binary.jc(predict, target)
+        hd = metric.binary.hd95(predict, target)
+        asd = metric.binary.asd(predict, target)
+        precision_ = metric.binary.precision(predict, target)
+        recall_ = metric.binary.recall(predict, target)
+
+        return dice, jc, hd, asd
+    elif predict.sum() == 0 and target.sum() == 0:
+        return 1, 0, 0, 0
+    else:
+        return 0, 0, 0, 0
+
+
+def all_metric(predict, target):
+    if torch.is_tensor(predict):
+        predict = predict.detach().cpu().numpy()
+    if torch.is_tensor(target):
+        target = target.detach().cpu().numpy()
+    ja1 = jaccard_to_binary(predict, target)
+    acc = accuracy(predict, target)
+    preci = precision_to_binary(predict, target)
+    reca = recal_to_binary(predict, target)
+    fs = fscore(predict, target)
+    # dsc = dice_to_binary(predict, target)
+    dsc = per_dice(predict, target)
+    spec = specificity_to_binary(predict, target)
+
+    dice, jc, hd, asd = calculate_metric_percase(predict, target)
+
+    reslut_dict = {"Accuracy": acc, "Precision": preci, "Recall": reca, "F1Score": fs, "Specificity": spec,
+                   "Dice": dsc, "Jaccard": ja1, "HausdorffDistance95": hd, "AverageSurfaceDistance": asd}
+    return reslut_dict
+
+
+if __name__ == '__main__':
+    print("*" * 30 + "\n |\t\tEnd Of Program\t\t|\n" + "*" * 30)
